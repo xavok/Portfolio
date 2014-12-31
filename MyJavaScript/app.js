5fe37c09da36d64fe0166a3ec0104f51cc33d2a1
xavok <xavoking@gmail.com>
2014-12-30 18:53:28 -0800

    time button

--- MyJavaScript/app.js
+++ MyJavaScript/app.js
@@ -43,19 +43,26 @@ var main = function() {
     currentDot.removeClass('active-dot');
     prevDot.addClass('active-dot');
   });
+	
+	
 
+$("#timeBtn").click(function()
+{
+	toggle_time('time');
+	document.getElementById('time').innerHTML = Date();
+});
 }
 
 $(document).ready(main);
 // this function changes html text to other text
-function hhh() {
-                document.getElementById('h2').innerHTML = "HAHAAHAH";
+function zoroInfo() {
+                document.getElementById('zoro').innerHTML = "<h1>Zoro</h1><br />He was the first member to join the Straw Hat Pirates, and, to date, is considered the largest threat and most dangerous member in the crew after Luffy. His fame as a master swordsman and his great strength, along with the actions of his captain, sometimes led others to believe he was the true captain of the crew earlier on. He is one of the top three fighters in the crew alongside Luffy and Sanji, and his dream is to become the greatest swordsman in the world. Zoro was regarded as one of 'The Eleven Supernovas', the eleven rookie pirates with bounties over 100,000,000 to have reached the Red Line before the two year time skip, with a current bounty of 120,000,000.";
             }
-function newFunction() {
-    document.getElementById('demo').innerHTML = "CHAAAAANGED";
+function luffyInfo() {
+    document.getElementById('luffy').innerHTML = "<h1>Luffy</h1><br /> also known as 'Straw Hat', is a pirate and the main protagonist of the anime and manga, One Piece. He is the son of the Revolutionary Army's commander, Monkey D. Dragon, the grandson of the famed Marine, Monkey D. Garp, the foster son of a mountain bandit, Curly Dadan, and the adopted brother of the late 'Fire Fist' Portgas D. Ace and the revolutionary Sabo. His lifelong dream is to become the Pirate King by finding the legendary treasure left behind by the previous Pirate King, Gol D. Roger. He believes that being Pirate King means having the most freedom in the world. He has eaten the Gomu Gomu no Mi. As the founder and captain of the Straw Hat Pirates, he is the first member of the crew, as well as one of its top three fighters.";
 }
-function h4New() {
-    document.getElementById('change').innerHTML = "CHANGESSSSSSS";
+function sanjiInfo() {
+    document.getElementById('sanji').innerHTML = "<h1>Sanji</h1><br />Sanji is a pirate and the cook of the Straw Hat Pirates, and the former sous chef of Baratie. He is officially the fifth member of the crew and the fourth to join, doing so at the end of the Baratie Arc. Since he was born in North Blue, he is the first Straw Hat not to originate from East Blue. He is one of the Monster Trio in the crew alongside Luffy and Zoro. His dream is to find the rumored All Blue, which is where East Blue, West Blue, North Blue, and South Blue meet along with their wildlife, a chef's paradise. He has a bounty of Beli77,000,000."
 }
 
 // This function changes images after you click on it
@@ -101,3 +108,15 @@ function h4New() {
                 document.getElementById("math3").innerHTML = d;
 
             }
+			
+// Function to toogle time button
+function toggle_time(id) {
+    var time = document.getElementById(id);
+    if (time.style.display == 'block' || time.style.display == '') {
+        time.style.display = 'none';
+    }
+    else 
+    {
+        time.style.display = 'block';
+    }
+}
